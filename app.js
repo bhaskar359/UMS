8ff30fb06fd9136579a49806e9db0a70c548ae98
bhaskar359 <bhaskarnarendra359@gmail.com>
2021-11-07 18:36:14 +0530

    created dBpool and env file to config

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ app.use(express.static('public'));
 app.engine('hbs', exphbs({extname: ".hbs"}));
 app.set('view engine', 'hbs');
 
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASS,
+    database        : process.env.DB_NAME
+});
+
+pool.getConnection((err,connection) => {
+    if(err) throw err;
+    console.log('Connected as ID '+ connection.threadId);
+});
+
 app.get('',(req,res) => {
     res.render('home');
 });
