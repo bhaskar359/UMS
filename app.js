05b089219b51348922007d11985deb8c3eaa878f
bhaskar359 <bhaskarnarendra359@gmail.com>
2021-11-07 16:06:05 +0530

    adding middleware & layouts

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+app.use(express.static('public'));
+app.engine('hbs', exphbs({extname: ".hbs"}));
+app.set('view engine', 'hbs');
+
+app.get('',(req,res) => {
+    res.render('home');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
