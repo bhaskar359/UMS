894f13d14968e1b16507138db3a8fffebd5a2db0
bhaskar359 <bhaskarnarendra359@gmail.com>
2021-11-11 20:55:07 +0530

    updating little things...

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,11 +5,11 @@ const userController = require('../controllers/userController');
 
 router.get('/',userController.view);
 router.post('/',userController.find);
-router.get('/:id',userController.delete);
 router.get('/adduser',userController.form);
 router.post('/adduser',userController.create);
 router.get('/edituser/:id',userController.edit);
 router.post('/edituser/:id',userController.update);
+router.get('/:id',userController.delete);
 
 // router.get('',(req,res) => {
 //     res.render('home');
